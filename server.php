8640fc81ed62dcd93536a2b4d12121267ca239df
panlatent <panlatent@gmail.com>
2016-05-24 06:47:54 +0800

    Add Daemon

--- server.php
+++ server.php
@@ -13,6 +13,32 @@ if ('cli' != php_sapi_name()) {
 
 require __DIR__ . '/vendor/autoload.php';
 
+$cmdOptions = getopt('s::');
+$signal = $cmdOptions['s'] ?? 'start';
+
+try {
+    $daemon = new \Panlatent\Server\Daemon(__FILE__);
+    switch ($signal) {
+        case 'start':
+            $daemon->start();
+            break;
+        case 'stop':
+            $daemon->stop();
+            exit(0);
+        case 'status':
+            if ($daemon->status()) {
+                die('Server is Running');
+            } else {
+                die('Server is Stopped');
+            }
+            break;
+        default:
+            die('Bad command line parameter');
+    }
+} catch (\Exception $e) {
+    die($e->getMessage());
+}
+
 $server = new Panlatent\Server\Http\Server();
 $server->bind('127.0.0.1', 10011);
 $server->listen();
--- src/Server/Daemon.php
+++ src/Server/Daemon.php
@@ -11,4 +11,60 @@ namespace Panlatent\Server;
 
 class Daemon {
 
+    protected $key;
+
+    protected $shmId;
+
+    public function __construct($daemonFile)
+    {
+        if (false === ($this->key = ftok($daemonFile, 'c'))) {
+            throw new Exception('Daemon process startup failed: ftok() error');
+        }
+    }
+
+    public function start()
+    {
+        if ($this->status()) {
+            throw new Exception('Daemon process has been running');
+        }
+
+        switch (pcntl_fork()) {
+            case 0:
+                $this->shmId = shmop_open($this->key, 'c', 0660, 4);
+                posix_setsid();
+                shmop_write($this->shmId, pack('i1', posix_getpid()), 0);
+                break;
+            case -1:
+                throw new Exception('Daemon process startup failed');
+            default:
+                exit(0);
+        }
+    }
+
+    public function status()
+    {
+        if (false === ($this->shmId = @shmop_open($this->key, 'a', 0660, 4))) {
+            return false;
+        }
+
+        $pid = unpack('i1', shmop_read($this->shmId, 0, 4))[1];
+
+        return posix_getpgid($pid) ? $pid : false;
+    }
+
+    public function stop()
+    {
+        if ( ! ($pid = $this->status())) {
+            throw new Exception('Daemon process has been stopped');
+        }
+
+        posix_kill($pid, SIGHUP);
+        shmop_close($this->shmId);
+    }
+
+    public function restart()
+    {
+
+    }
+
 }
