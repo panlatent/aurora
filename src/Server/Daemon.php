c08899185e70b7ed6d0586039312680c39eecf04
panlatent <panlatent@gmail.com>
2016-06-04 00:20:01 +0800

    Add EasyShm library

--- src/Server/Daemon.php
+++ src/Server/Daemon.php
@@ -3,17 +3,17 @@
  * Server
  *
  * @author  panlatent@gmail.com
- * @link    https://github.com/panlatent/seven-server
- * @license https://opensource.org/licenses/MIT
  */
 
 namespace Panlatent\Server;
+
+use Panlatent\EasyShm\Shm;
 
 class Daemon {
 
     protected $key;
 
-    protected $shmId;
+    protected $shm;
 
     public function __construct($daemonFile)
     {
@@ -21,42 +21,21 @@ class Daemon {
             throw new Exception('Daemon process startup failed: ftok() error');
         }
 
-        $this->shmId = shmop_open($this->key, 'c', 0660, 4);
+        $this->shm = new Shm($this->key, 4, 0660);
     }
 
     public function __destruct()
     {
         if (false === $this->status()) {
-            shmop_close($this->shmId);
+            $this->shm->delete();
         }
     }
 
-    public function start()
+    public function status()
     {
-        if ($this->status()) {
-            throw new Exception('Daemon process has been running');
-        }
+        $pid = unpack('i1', $this->shm->read(0, 4))[1];
 
-        switch (pcntl_fork()) {
-            case 0:
-                posix_setsid();
-                shmop_write($this->shmId, pack('i1', posix_getpid()), 0);
-                break;
-            case -1:
-                throw new Exception('Daemon process startup failed');
-            default:
-                exit(0);
-        }
-    }
-
-    public function stop()
-    {
-        if ( ! ($pid = $this->status())) {
-            throw new Exception('Daemon process has been stopped');
-        }
-
-        posix_kill($pid, SIGHUP);
-        shmop_write($this->shmId, pack('i1', 0), 0);
+        return $pid != 0 || posix_getpgid($pid) ? $pid : false;
     }
 
     public function restart()
@@ -68,11 +47,32 @@ class Daemon {
         $this->start();
     }
 
-    public function status()
+    public function stop()
     {
-        $pid = unpack('i1', shmop_read($this->shmId, 0, 4))[1];
+        if ( ! ($pid = $this->status())) {
+            throw new Exception('Daemon process has been stopped');
+        }
 
-        return $pid != 0 || posix_getpgid($pid) ? $pid : false;
+        posix_kill($pid, SIGHUP);
+        $this->shm->write(0, pack('i1', 0));
+    }
+
+    public function start()
+    {
+        if ($this->status()) {
+            throw new Exception('Daemon process has been running');
+        }
+
+        switch (pcntl_fork()) {
+            case 0:
+                posix_setsid();
+                $this->shm->write(0, pack('i1', posix_getpid()));
+                break;
+            case -1:
+                throw new Exception('Daemon process startup failed');
+            default:
+                exit(0);
+        }
     }
 
 }
